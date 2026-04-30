dec1100112cb3eba92caa81c066376f7b4ce0bbc
stefanciutac <stefanciutac6@gmail.com>
2026-04-30 16:16:34 +0100

    FEAT: Implemented left and right shifters

--- src/Logic.hs
+++ src/Logic.hs
@@ -1,7 +1,16 @@
 module Logic
-    ( 
-    ) where
+  ( rightShift,
+    leftShift,
+  )
+where
 
+import Control.Arrow (ArrowChoice (left))
 import Gates
+import Register
 
--- ALU logic will go here
+-- Shifters
+rightShift :: [Bool] -> Bool -> ([Bool], Bool)
+rightShift bus carry = ([carry] ++ [bus !! i | i <- [0 .. 6]], bus !! 7)
+
+leftShift :: [Bool] -> Bool -> ([Bool], Bool)
+leftShift bus carry = ([bus !! i | i <- [1 .. 7]] ++ [carry], bus !! 0)
